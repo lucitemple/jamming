c31543336a953f9403b0ed884b9f906cb0461693
Luci Temple <lucitemple@yahoo.com.au>
2021-03-21 23:02:18 +1100

    Add method to Track component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,14 +3,17 @@ import './Track.css';
 
 
 export default function Track() {
-    const isRemoval = true;
+
+  const renderAction = (isRemoval) => {
+          <button className="Track-action">{isRemoval ? '+' : '-'}</button>
+  }
   return (
     <div className="Track">
       <div className="Track-information">
         <h3>track name will go here</h3>
         <p>track artist will go here | track album will go here</p>
       </div>
-      <button className="Track-action">{isRemoval ? '+' : '-'}</button>
+      {{renderAction}}
     </div>
   );
 }
