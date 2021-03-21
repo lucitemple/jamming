d41b133e26c48d7568b3c02c2672d87d82a7ced2
Luci Temple <lucitemple@yahoo.com.au>
2021-03-21 22:25:02 +1100

    Add TrackList component

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Track from '../Track';
+import './TrackList.css/';
+
+export default function TrackList() {
+    return(
+<div className="TrackList">
+    {/* You will add a map method that renders a set of Track components */}
+    <Track />
+</div>
+    )
+}
