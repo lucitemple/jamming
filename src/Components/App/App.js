cbff22566e709151d5662df7960de5be1699c260
Luci Temple <lucitemple@yahoo.com.au>
2021-03-29 12:53:39 +1100

    Hardcode playlistName and playlistTracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,8 +14,13 @@ class App extends React.Component {
       searchResults: [
         { name: "name1", artist: "artist1", album: "album1", id: 1 },
         { name: "name2", artist: "artist2", album: "album2", id: 2 },
-  
-        { name: "name3", artist: "artist3", album: "album3", id: 1 },
+        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+      ],
+      playlistName: "Playlist Name",
+      playlistTracks: [
+        { name: "name4", artist: "artist4", album: "album4", id: 4 },
+        { name: "name5", artist: "artist5", album: "album5", id: 5 },
+        { name: "name6", artist: "artist6", album: "album6", id: 6 },
       ],
     };
   }
@@ -30,7 +35,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
