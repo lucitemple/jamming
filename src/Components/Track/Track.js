a0b8fc5b47737a928631e7ef84512eabb036677b
Luci Temple <lucitemple@yahoo.com.au>
2021-03-31 15:27:39 +1100

    Fix styling

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -39,7 +39,7 @@ class Track extends React.Component {
         <div className="Track-information">
           <h3>{this.props.track.name}</h3>
           <p >
-            {this.props.track.artist} | {this.props.track.album}{" "}
+            {this.props.track.artist} | {this.props.track.album}
           </p>
         </div>
         {this.renderAction()}
