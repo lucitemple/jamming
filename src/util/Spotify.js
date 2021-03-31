11520924f542a001283b69d961d576ab7649fa36
Luci Temple <lucitemple@yahoo.com.au>
2021-03-31 13:33:32 +1100

    Fix spotify fetch links

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -62,23 +62,19 @@ const Spotify = {
     return fetch("https://api.spotify.com/v1/me", {
       headers: headers,
     })
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((jsonResponse) => {
         userId = jsonResponse.id;
-        return fetch("https://api.spotify.com/v1/users/{userId}/playlists", {
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
           headers: headers,
           method: "POST",
-          body: JSON.stringify({ name: name }),
+          body: JSON.stringify({ name: name })
         })
-          .then((response) => {
-            return response.json();
-          })
+          .then((response) => response.json())
           .then((jsonResponse) => {
             const playlistId = jsonResponse.id;
             return fetch(
-              `https://api.spotify.com//v1/users/${userId}/playlists/${playlistId}/tracks`,
+              `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
               {
                 headers: headers,
                 method: "POST",
