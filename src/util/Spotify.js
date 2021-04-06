087aaaed87d1d52b490b0f4906adedffcbcf8dcb
Luci Temple <lucitemple@yahoo.com.au>
2021-04-06 14:35:57 +1000

    Fix api

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,8 @@
 let accessToken;
-const clientId = "c3f7f34cdf5c4f3e831b91ca134bc6d1";
+const clientId = `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}`;
 const redirectUri = "https://lucitemple.github.io/jamming/"; // Must match spotify app setting exactly & include trailing slash.
+
+console.log(clientId);
 
 const Spotify = {
   getAccessToken() {
