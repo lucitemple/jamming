a8ebbb8aaaf7c7a34d278507c8fc99a7aa1b46b4
Luci Temple <lucitemple@yahoo.com.au>
2021-04-22 10:24:13 +1000

    Troubleshooting new feature

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -42,9 +42,14 @@ class App extends React.Component {
   }
 
   savePlaylist() {
+    console.log(`line 45 App.js`); //printing - before error
     let trackUris = this.state.playlistTracks.map((track) => track.uri);
+    console.log(`line 47 App.js ${trackUris}`); // printing before error
     if (trackUris && trackUris.length) {
+      console.log(`line 49 App.js ${trackUris}`); //printing before error
+      // below line is the problem 'Uncaught TypeError Cannot read property 'then' of undefined
       Spotify.savePlayList(this.state.playlistName, trackUris).then(() => {
+        console.log(`line 52 App.js ${trackUris}`); //not printing
         this.setState({
           playlistName: "New Playlist Name",
           playlistTracks: [],
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,8 @@
 let accessToken;
+let userId;
 const clientId = `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}`;
-const redirectUri = "https://lucitemple.github.io/jamming/"; // Must match spotify app setting exactly & include trailing slash.
+const redirectUri =
+  /* "https://lucitemple.github.io/jamming/" */ "http://localhost:3000"; // Must match spotify app setting exactly & include trailing slash.
 
 const Spotify = {
   getAccessToken() {
@@ -23,6 +25,32 @@ const Spotify = {
       window.location = accessUrl;
     }
   },
+
+  getCurrentUserId() {
+    if (userId) {
+      return userId;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    return fetch("https://api.spotify.com/v1/me", {
+      headers: headers,
+    })
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        userId = jsonResponse.id;
+        console.log(`line 45 ${userId}`); //prints after error messages
+         return userId;
+      })
+      .catch(function (err) {
+        console.log("Fetch problem line 49: " + err.message); //doesn't print
+      });;
+  },
+
+  getUserPlaylists() {},
 
   search(term) {
     const accessToken = Spotify.getAccessToken();
@@ -57,32 +85,59 @@ const Spotify = {
     const headers = {
       Authorization: `Bearer ${accessToken}`,
     };
-    let userId;
+
+    Spotify.getCurrentUserId().then((response) => {
+      userId = response;
+      console.log(`line 91 ${userId}`); // prints after error messages
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+        headers: headers,
+        method: "POST",
+        body: JSON.stringify({ name: name }),
+      })
+        .then((response) => response.json())
+        .then((jsonResponse) => {
+          const playlistId = jsonResponse.id;
+          console.log(`line 100 ${playlistId}`); //prints after error messages
+          return fetch(
+            `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+            {
+              headers: headers,
+              method: "POST",
+              body: JSON.stringify({ uris: trackUris }),
+            }
+          );
+        })
+        .catch(function (err) {
+          console.log("Fetch problem: " + err.message); //doesn't print
+        });
+    });
+
+    /*     let userId;
 
     return fetch("https://api.spotify.com/v1/me", {
       headers: headers,
     })
       .then((response) => response.json())
       .then((jsonResponse) => {
-        userId = jsonResponse.id;
-        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-          headers: headers,
-          method: "POST",
-          body: JSON.stringify({ name: name })
-        })
-          .then((response) => response.json())
-          .then((jsonResponse) => {
-            const playlistId = jsonResponse.id;
-            return fetch(
-              `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
-              {
-                headers: headers,
-                method: "POST",
-                body: JSON.stringify({ uris: trackUris }),
-              }
-            );
-          });
-      });
+        userId = jsonResponse.id; */
+
+    /*     return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+      headers: headers,
+      method: "POST",
+      body: JSON.stringify({ name: name }),
+    })
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        const playlistId = jsonResponse.id;
+        return fetch(
+          `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+          {
+            headers: headers,
+            method: "POST",
+            body: JSON.stringify({ uris: trackUris }),
+          }
+        );
+      }); */
   },
 };
 
